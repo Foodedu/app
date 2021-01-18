8e976e6a54703ef29c08ac4d6e252cb711c4612c
TrungNguyen1208 <thanhtrungnguyen1208@gmail.com>
2021-01-18 23:21:39 +0700

    Add font, color

---
+++ lib/general/contanst/app_font.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppFont {
+  static const kRegularTextStyle = TextStyle(
+    color: Color.fromRGBO(51, 51, 51, 1),
+    fontFamily: "AvenirNextLTPro",
+    fontSize: 17.0,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const kSemiBoldTextStyle = TextStyle(
+    color: Color.fromRGBO(51, 51, 51, 1),
+    fontFamily: "AvenirNextLTPro",
+    fontSize: 17.0,
+    fontWeight: FontWeight.w600,
+  );
+}
